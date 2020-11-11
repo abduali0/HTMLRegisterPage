b29637616681d5aa45fa7fd5d4f4ec3a3052b749
Ali, Abdullah <abduali@iu.edu>
2020-11-11 00:08:27 -0500

    initial

---
+++ Code/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+			
+	// Pop up a window that says "Hello, please tell me you're not a robot."
+	alert("Well you must love code, else you wouldn't be here. ");
+				
+}); // end of $(document).ready()
